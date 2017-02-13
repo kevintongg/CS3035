c9809742358704c9c6b69457d1e33f8bff0d6983
Kevin Tong <kcr1230@hotmail.com>
2017-02-12 22:45:00 -0800

    Homework

--- Homework 1/main.js
+++ Homework 1/main.js
@@ -9,10 +9,13 @@ var map = [
 [" "," "," "," "," "," "," "," "]
 ]
 
+var hitPoints = 23;
+
 var printMap = function(map) {
   for (var i = 0; i < map.length; i++) {
     document.write("_");
     for (var j = 0; j < map[i].length; j++) {
+      map[i][j] = "_";
       document.write(map[i][j]);
     }
     document.write("</br>");
@@ -29,16 +32,37 @@ printMap(map);
 var monster = function(name, health, prize) {
   name = name;
   health = health;
-  prize = prize
+  prize = prize;
 };
+
+// var location = function(position) {
+//   position = map[Math.floor(Math.random() * map.length)][Math.floor(Math.random() * map.length)];
+// }
+
+// var randomSpot = function(map) {
+//   var random = Math.floor((Math.random() * (map.length)));
+//   return map[random][Math.floor((Math.random()) * (map[random].length)]
+// }
+
+var adventurer = function(health, position, prize) {
+  health = health;
+  position = location();
+  prize = prizes[Math.floor(Math.random() * prizes.length)];
+}
+
+var prizes = ["Invisibility Potion", "Excalibur (Sword)", "Sword of a Thousand Truths", "Lambent Light (Rapier)" , "Elucidator (Sword)", "Dark Repulsor (Sword)", "Titanium Exoskeleton"]
+
+function damageCalculator() {
+    var x = Math.floor((Math.random() * 10) + 1);
+}
+
+function randomPrize(prizes) {
+  return prizes(Math.floor(Math.random() * prizes.length));
+}
 
 document.write("hello!");
 
 map[1][2] = monster("prize",100,"trophy");
+map[4][4] = adventurer(hitPoints, location(), randomPrize(prizes));
 
 document.write("hello"+map[1][2]["name"]);
-
-// function damageCalculator() {
-//     var x = Math.floor((Math.random() * 10) + 1);
-//     document.write(x);
-// }
