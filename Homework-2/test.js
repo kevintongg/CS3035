685e97f81a1e6e61f0d9366aa2b7bbf6bb6d59dd
Kevin Tong <kcr1230@hotmail.com>
2017-03-15 15:51:30 -0700

    Homework

--- Homework-2/test.js
+++ Homework-2/test.js
@@ -1,4 +1,4 @@
-const people = require('./people');
+// const people = require('./people');
 
 const restaurants = [
   {
@@ -104,16 +104,26 @@ CenteredTextCell.prototype = Object.create(TextCell.prototype);
 CenteredTextCell.prototype.draw = function (width, height) {
   const result = [];
   for (let i = 0; i < height; i++) {
-    // let line = this.text[i] || '';
-    const line = this.text[i] || '';
+    const line = this.text[i] === '[object Object]' ? '' : this.text[i];
     result.push(repeat(' ', Math.floor((width - line.length) / 2)) + line + repeat(' ', Math.floor((width - line.length) / 2)));
   }
-
-  // for (let i = 0; i < height; i++) {
-  //   console.log(result.length);
-  //   console.log(typeof result[1]);
-  // }
   return result;
+};
+
+function BorderedCell(inner) {
+  this.inner = inner;
+}
+
+BorderedCell.prototype.minWidth = function () {
+  return this.inner.minWidth();
+};
+
+BorderedCell.prototype.minHeight = function () {
+  return this.inner.minHeight() + 1;
+};
+
+BorderedCell.prototype.draw = function (width, height) {
+  return this.inner.draw(width, height - 1).concat(repeat('-', width).concat(repeat('|', height)));
 };
 
 function centeredTable(data) {
@@ -129,9 +139,9 @@ function centeredTable(data) {
   return [headers].concat(body);
 }
 
-function dataTable(data) {
+function borderedTable(data) {
   const keys = Object.keys(data[0]);
-  const headers = keys.map(name => new UnderlinedCell(new TextCell(name)));
+  const headers = keys.map(name => new BorderedCell(new TextCell(name)));
   const body = data.map(row => keys.map((name) => {
     const value = row[name];
     if (typeof value === 'number') {
@@ -142,6 +152,21 @@ function dataTable(data) {
   return [headers].concat(body);
 }
 
+// function dataTable(data) {
+//   const keys = Object.keys(data[0]);
+//   const headers = keys.map(name => new UnderlinedCell(new TextCell(name)));
+//   const body = data.map(row => keys.map((name) => {
+//     const value = row[name];
+//     if (typeof value === 'number') {
+//       return new RTextCell(String(value));
+//     }
+//     return new TextCell(String(value));
+//   }));
+//   return [headers].concat(body);
+// }
+
 console.log('Problem 1:');
 console.log(drawTable(centeredTable(restaurants)));
-// console.log(drawTable(dataTable(people)));
+console.log('\nProblem 2:');
+console.log(drawTable(borderedTable(restaurants)));
+// console.log('\n' + drawTable(dataTable(people)));
