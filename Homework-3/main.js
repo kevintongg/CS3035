739d50d23ea9386aca468576c77bc97fd7d1f2f9
Kevin Tong <kcr1230@hotmail.com>
2017-04-02 22:16:35 -0700

    Homework

--- Homework-3/main.js
+++ Homework-3/main.js
@@ -13,7 +13,7 @@ const monsterList = [
   'Archimonde', // 5
   'Gul\'dan', // 6
 ];
-let prizes = [
+const prizes = [
   'Black Ice [Polearm]', // 0
   'Excalibur [Sword]', // 1
   'Sword of a Thousand Truths [Sword]', // 2
@@ -23,7 +23,7 @@ let prizes = [
   'Titanium Exoskeleton [Armor]', // 6
   'Purple Floppyslapper [Bludgeon]', // 7
 ];
-let explorationMessages = [
+const explorationMessages = [
   'You see something bright. Ooh! Shiny!', // 0
   'You see a treasure chest. To your surprise, this is the item you have been wanting all your life!', // 1
   'You find an item that could really save your life when you really need it.', // 2
@@ -36,7 +36,7 @@ const encounterMessages = [
   'You can feel an extraordinary aura.', // 2
   'You have a feeling that this will be one of the hardest fights of your life.', // 3
 ];
-let potionMessages = [
+const potionMessages = [
   'You feel reinvigorated.', // 1
   'You feel refreshed.', // 2
   'You feel like you could do things you could not do before!', // 3
@@ -128,15 +128,15 @@ const map = [
   // 0,0 0,1 0,2 0,3 0,4 0,5 0,6 0,7
   ['—', '—', '—', '—', '—', '—', '—', '—'],
   // 1,0 1,1 1,2 1,3 1,4 1,5 1,6 1,7
-  ['—', '—', 'monster', '—', '—', '—', 'prize', '—'],
+  ['—', '—', '—', '—', '—', '—', '—', '—'],
   // 2,0 2,1 2,2 2,3 2,4 2,5 2,6 2,7
   ['—', '—', '—', '—', '—', '—', '—', '—'],
   // 3,0 3,1 3,2 3,3 3,4 3,5 3,6 3,7
-  ['—', '—', '—', '—', '—', 'potion', '—', '—'],
+  ['—', '—', '—', '—', '—', '—', '—', '—'],
   // 4,0 4,1 4,2 4,3 4,4 4,5 4,6 4,7
-  ['—', '—', '—', 'potion', '—', '—', 'monster', '—'],
+  ['—', '—', '—', '—', '—', '—', '—', '—'],
   // 5,0 5,1 5,2 5,3 5,4 5,5 5,6 5,7
-  ['—', '—', '—', 'monster', '—', '—', '—', '—'],
+  ['—', '—', '—', '—', '—', '—', '—', '—'],
   // 6,0 6,1 6,2 6,3 6,4 6,5 6,6 6,7
   ['—', '—', '—', '—', `<strong style='font-size: 12pt;'>${hero.name}</strong>`, '—', '—', '—'],
   // 7,0 7,1 7,2 7,3 7,4 7,5 7,6 7,7
@@ -157,7 +157,7 @@ function events() {
         prize1 = true;
         break;
       }
-      prizes = prizes.filter(item => item !== treasure);
+      remove(prizes, treasure);
       remove(explorationMessages, message);
       $('#prizes').html(hero.loot.join('<br/>'));
       if ($('#yes').is(':visible')) {
@@ -176,10 +176,10 @@ function events() {
       if (hero.loot[i] === undefined) {
         hero.loot.push(treasure);
         hero.prizeCounter++;
-        prize1 = true;
+        prize2 = true;
         break;
       }
-      prizes = prizes.filter(item => item !== treasure);
+      remove(prizes, treasure);
       remove(explorationMessages, message);
       $('#prizes').html(hero.loot.join('<br/>'));
       if ($('#yes').is(':visible')) {
@@ -188,7 +188,7 @@ function events() {
       $('#prizeCounter').html(`Your current number of prizes: ${hero.prizeCounter}`);
     }
   }
-  if (hero.xCoordinate === 4 && hero.yCoordinate === 3) {
+  if ((hero.xCoordinate === 4 && hero.yCoordinate === 3) && potion1 === false) {
     const message = randomElement(potionMessages);
     const event = `Obtained a potion at [${hero.xCoordinate}, ${hero.yCoordinate}]`;
     const regen = potionCalculator();
@@ -196,8 +196,9 @@ function events() {
     log.push(`${message}<br/>${event}`);
     $('#log-info').html(log.join('<br/>'));
     remove(potionMessages, message);
+
   }
-  if (hero.xCoordinate === 3 && hero.yCoordinate === 5) {
+  if ((hero.xCoordinate === 3 && hero.yCoordinate === 5) && potion2 === false) {
     const message = randomElement(potionMessages);
     const event = `Obtained a potion at [${hero.xCoordinate}, ${hero.yCoordinate}]`;
     const regen = potionCalculator();
@@ -205,26 +206,46 @@ function events() {
     log.push(`${message}<br/>${event}`);
     $('#log-info').html(log.join('<br/>'));
     remove(potionMessages, message);
+    potion2 = true;
   }
 }
 
 function mapInfo() {
+  if (lastX === 4 && lastY === 6) {
+    map[4][6] = '<i class="monster">Monster</i>';
+  }
+  if (lastX === 5 && lastY === 3) {
+    map[5][3] = '<i class="monster">Monster</i>';
+  }
+  if (lastX === 1 && lastY === 2) {
+    map[1][2] = '<i class="monster">Monster</i>';
+  }
   if (lastX === 6 && lastY === 1) {
-    map[6][1] = '<i>Prize</i>';
+    map[6][1] = '<i class="prize">Prize</i>';
   }
   if (lastX === 1 && lastY === 6) {
-    map[lastX][lastY] = '<i>Prize</i>';
+    map[1][6] = '<i class="prize">Prize</i>';
   }
   if (lastX === 4 && lastY === 3) {
-    map[lastX][lastY] = '<i>Potion</i>';
+    map[4][3] = '<i class="potion">Potion</i>';
   }
   if (lastX === 3 && lastY === 5) {
-    map[lastX][lastY] = '<i>Potion</i>';
+    map[3][5] = '<i class="potion">Potion</i>';
   }
-  if (!(lastX === 6 && lastY === 1) && !(lastX === 1 && lastY === 6) &&
-    !(lastX === 4 && lastY === 3) && !(lastX === 3 && lastY === 5)) {
-    map[lastX][lastY] = '<i>Explored</i>';
+  if (!(lastX === 4 && lastY === 6) && !(lastX === 5 && lastY === 3) &&
+    !(lastX === 1 && lastY === 2) && !(lastX === 6 && lastY === 1) &&
+    !(lastX === 1 && lastY === 6) && !(lastX === 4 && lastY === 3) &&
+    !(lastX === 3 && lastY === 5)) {
+    map[lastX][lastY] = '<i class="explored">Explored</i>';
   }
+
+  // for (let i = 0; i < map.length; i++) {
+  //   for (let j = 0; j < map[i].length; j++) {
+  //     if (map[i][j] === '<i>Explored</i>') {
+  //       map[i][j] = '<i style="font-size: 12px;">Explored</i>';
+  //     }
+  //   }
+  // }
 }
 
 // 4,6 monster
@@ -460,7 +481,7 @@ $(document).ready(() => {
           hero.loot.push(treasure);
           break;
         }
-        prizes = prizes.filter(item => item !== treasure);
+        remove(prizes, treasure);
         hero.prizeCounter++;
       }
     }
@@ -479,7 +500,7 @@ $(document).ready(() => {
     move();
     exploration();
     if (!monsters[0].alive && !monsters[1].alive && !monsters[2].alive) {
-      document.write('<style>#victory { position:fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); } #refresh { position: absolute; width: 300px; height: 300px; bottom: 0px; right: 25%; left: 50%; margin-left: -150px</style><link rel="stylesheet" href="bootstrap.css"><div class="container"><h2 id="victory" class="text-center">You\'ve won!<br/>Please refresh the page to play again!</h2><div id="refresh" class="text-center"><button id="refreshButton" class="btn btn-primary text-center">Refresh</button></div></div>');
+      document.write('<style>#victory { position:fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); } #refresh { position: absolute; width: 300px; height: 300px; bottom: 0; right: 25%; left: 50%; margin-left: -150px</style><link rel="stylesheet" href="bootstrap.css"><div class="container"><h2 id="victory" class="text-center">You\'ve won!<br/>Please refresh the page to play again!</h2><div id="refresh" class="text-center"><button id="refreshButton" class="btn btn-primary text-center">Refresh</button></div></div>');
       $('#refreshButton').click(() => {
         location.reload();
       });
