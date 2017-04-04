f09ad87d9d1af02e24544a46ff107c06fd8020d7
Kevin Tong <kcr1230@hotmail.com>
2017-04-03 23:38:21 -0700

    Stuff

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,15 @@
-const figlet = require('figlet');
+const array = [0.00624, 0.00125, 0.00120];
 
+console.log(array);
 
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] > array[i]) {
+      const temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
+    }
+  }
+}
+
+console.log(array);
