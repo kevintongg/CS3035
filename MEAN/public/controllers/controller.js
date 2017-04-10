48dfbf6b241e29bda03b242f8b6c31633e9bdea7
Kevin Tong <kcr1230@hotmail.com>
2017-04-10 13:42:45 -0700

    MEAN progress

--- MEAN/public/controllers/controller.js
+++ MEAN/public/controllers/controller.js
@@ -1,3 +1,8 @@
-function AppCtrl() {
+function AppCtrl($scope, $http) {
   console.log('Hello from controller');
+
+  $http.get('/contactlist').success((response) => {
+    console.log('I got the data I requested');
+    $scope.contactList = response;
+  });
 }
--- MEAN/server.js
+++ MEAN/server.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+const fullPath = path.join(__dirname, '/public');
+
+app.use(express.static(fullPath));
+
+app.get('/contactlist', (request, response) => {
+  console.log('I received a GET request');
+
+  const person1 = {
+    name: 'Tim',
+    email: 'tim@tim.com',
+    number: '(111) 111-1111',
+  };
+
+  const person2 = {
+    name: 'Emily',
+    email: 'emily@emily.com',
+    number: '(222) 222-2222',
+  };
+
+  const person3 = {
+    name: 'John',
+    email: 'john@john.com',
+    number: '(333) 333-3333',
+  };
+
+  const contactlist = [person1, person2, person3];
+  response.json(contactlist);
+});
 
 app.listen(3000);
 console.log('Server running on port 3000');
